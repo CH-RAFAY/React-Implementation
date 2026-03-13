bdbccfde3ebb112143ef939f2d2b067d4710d924
CH-RAFAY <chrafayather@gmail.com>
2026-03-13 21:20:58 +0500

    Components Linking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.js';
+import Coffee from './Coffee.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Initializing react with strong " Passion "!</h1>
-    </div>
+
+    <>
+      <Coffee />
+          <h1>Initializing react with strong " Passion "!</h1>
+
+          <p>React is a powerful JavaScript library for building user interfaces. It allows developers to create reusable UI components and efficiently update the DOM. With React, you can build dynamic and interactive web applications with ease.</p>
+    </>
+
+
+
   );
 }
+
 
 export default App;
---
+++ src/Coffee.js
@@ -0,0 +1,10 @@
+function Coffee(){
+    return(
+        <div>
+            <h1>Welcome to Coffee Shop</h1>
+            <p>Enjoy your cup of coffee!</p>
+        </div>
+    );
+}
+
+export default Coffee;

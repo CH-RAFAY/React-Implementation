b47f8cf97228f0042866eb5789d1b54b169fdd93
CH-RAFAY <chrafayather@gmail.com>
2026-03-13 20:40:37 +0500

    Initiating P

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Initializing react with strong " Passion "!</h1>
+    </div>
+  );
+}
+
+export default App;
